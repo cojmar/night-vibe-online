b98f6599c6312c1be189b121718ea20097a493d1
cojmar <cojmar@gmail.com>
2026-05-25 20:05:57 +0300

    wiz ok-ish

--- app/game.js
+++ app/game.js
@@ -1453,6 +1453,9 @@ releaseSkill2() {
       const newFireballRadius = fbRadius * aoeScale * lvlScale;
       const spawnX = this.player.x;
       const spawnY = weaponY;
+      const spdDiff = Math.max(0, this.player.spd - 200);
+      const fbLifeMult = 1 + Math.floor(spdDiff / 50) * 0.2;
+      const fbLife = Math.round(80 * fbLifeMult);
       let canSpawn = true;
       for (let p of this.projectiles) {
         if (p.type === 'fireball' && p.life > 0) {
@@ -1465,10 +1468,10 @@ releaseSkill2() {
         }
       }
       if (canSpawn) {
-        this.projectiles.push(new Projectile({ type: 'fireball', x: spawnX, y: spawnY, speed: 5, life: 80, maxLife: 80, color: cd.s2Color || '#e67e22', damage: this.player.atk * 1.0 * dmgMulti, critChance: 0.2, radius: newFireballRadius, traveled: 0, trailTimer: 0, trailPositions: [], ...projProps }));
+        this.projectiles.push(new Projectile({ type: 'fireball', x: spawnX, y: spawnY, speed: 5, life: fbLife, maxLife: fbLife, color: cd.s2Color || '#e67e22', damage: this.player.atk * 1.0 * dmgMulti, critChance: 0.2, radius: newFireballRadius, traveled: 0, trailTimer: 0, trailPositions: [], ...projProps }));
         this.spawnParticles(spawnX, spawnY, cd.s2Color || '#e67e22', 20 * aoeScale + charges * 5, 5);
       } else if (!this.queuedFireball) {
-        this.queuedFireball = { spawnX, spawnY, speed: 5, radius: newFireballRadius, color: cd.s2Color || '#e67e22', damage: this.player.atk * 1.0 * dmgMulti, ...projProps };
+        this.queuedFireball = { spawnX, spawnY, speed: 5, radius: newFireballRadius, color: cd.s2Color || '#e67e22', damage: this.player.atk * 1.0 * dmgMulti, ...projProps, fbLife };
       }
     } else if (skillType === 'Arrow Barrage' || this.player.classType === 'archer') {
       const maxArrowCount = 24;
@@ -2757,7 +2760,8 @@ releaseSkill2() {
           }
         }
         if (canSpawn) {
-          this.projectiles.push(new Projectile({ type: 'fireball', x: spawnX, y: spawnY, speed: qf.speed || 5, life: 80, maxLife: 80, color: qf.color || '#e67e22', damage: qf.damage || 1, critChance: 0.2, radius: qf.radius || 15, traveled: 0, trailTimer: 0, trailPositions: [], tx: qf.tx, ty: qf.ty, angle: qf.angle, facing: qf.facing }));
+          const qfLife = qf.fbLife || 80;
+          this.projectiles.push(new Projectile({ type: 'fireball', x: spawnX, y: spawnY, speed: qf.speed || 5, life: qfLife, maxLife: qfLife, color: qf.color || '#e67e22', damage: qf.damage || 1, critChance: 0.2, radius: qf.radius || 15, traveled: 0, trailTimer: 0, trailPositions: [], tx: qf.tx, ty: qf.ty, angle: qf.angle, facing: qf.facing }));
           this.spawnParticles(spawnX, spawnY, qf.color || '#e67e22', 15, 4);
           this.queuedFireball = null;
         }
