0ab4aa2e317bc1ed251c3510b67b408da3d070d7
cojmar <cojmar@gmail.com>
2026-05-26 11:00:09 +0300

    .

--- app/game.js
+++ app/game.js
@@ -1395,7 +1395,7 @@ export default class Game {
        this.spawnParticles(this.player.x, weaponY, cd.s1Color || '#e74c3c', 4, 3);
     } else if (skillType === 'Psionic Slash' || this.player.classType === 'magicgladiator') {
       const mgScale = 1 + (this.player.atk - cd.atk) * 0.005;
-      this.spawnProjectile({ type: 'slash', originX: this.player.x, originY: weaponY, life: 20, maxLife: 20, color: cd.s1Color || '#e74c3c', radius: 60 * mgScale * lvlScale, hitInner: 0, hitOuter: 80 * mgScale * lvlScale, damage: this.player.atk * 1.1, critChance: 0.12, ...projProps });
+      this.spawnProjectile({ type: 'psionic_slash', x: this.player.x, y: weaponY, vx: Math.cos(aimAngle) * 9, vy: Math.sin(aimAngle) * 9, angle: aimAngle, speed: 9, life: 30, maxLife: 30, color: cd.s1Color || '#e74c3c', radius: 45 * mgScale * lvlScale, damage: this.player.atk * 1.1, critChance: 0.12, ...projProps });
       this.spawnParticles(this.player.x + Math.cos(aimAngle) * 40, weaponY + Math.sin(aimAngle) * 40, cd.s1Color || '#e74c3c', 7, 4);
     } else {
       // Default fallback is Warrior Bash style
--- app/projectile.js
+++ app/projectile.js
@@ -6,7 +6,7 @@ export default class Projectile {
     if (!this.hitIds) this.hitIds = new Set();
     
     // Default initialization based on type
-    if (this.type === 'bolt' || this.type === 'fireball' || this.type === 'arrow') {
+    if (this.type === 'bolt' || this.type === 'fireball' || this.type === 'arrow' || this.type === 'psionic_slash') {
       if (this.vx === undefined && this.vy === undefined) {
         const a = Math.atan2(this.ty - this.y, this.tx - this.x);
         this.vx = Math.cos(a) * this.speed;
@@ -68,7 +68,28 @@ export default class Projectile {
       }
       if (this.life <= this.maxLife * 0.3) { this.life = 0; }
     }
-    else if (this.type === 'bolt' || this.type === 'fireball' || this.type === 'arrow') {
+    else if (this.type === 'psionic_slash') {
+      if (this.vx !== undefined) {
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+      }
+      this.traveled = (this.traveled || 0) + this.speed * dt;
+      this.currentRadius = (this.radius || 40) + this.traveled * 0.5;
+
+      for (let e of gameInstance.enemies) {
+        if (!e.alive || this.hitIds.has(e)) continue;
+        const hitX = this.x + Math.cos(this.angle) * this.currentRadius * 0.3;
+        const hitY = this.y + Math.sin(this.angle) * this.currentRadius * 0.3;
+        if (Math.hypot(hitX - e.x, hitY - e.y) < e.size + this.currentRadius * 0.6) {
+          if (!this.ownerId || (gameInstance.net && gameInstance.net.me && this.ownerId === gameInstance.net.me.info.user)) gameInstance.dealDamage(e, this.damage, this.critChance);
+          this.hitIds.add(e);
+          gameInstance.spawnParticles(e.x, e.y, this.color, 6, 3);
+        }
+      }
+      
+      if (this.life <= 0 || this.x < -100 || this.x > gameInstance.gameW + 100 || this.y < -100 || this.y > gameInstance.gameH + 100) this.life = 0;
+    }
+    else if (this.type === 'bolt' || this.type === 'fireball' || this.type === 'arrow' || this.type === 'psionic_slash') {
       if (this.vx !== undefined) {
         this.x += this.vx * dt;
         this.y += this.vy * dt;
@@ -276,6 +297,41 @@ export default class Projectile {
       ctx.beginPath(); ctx.arc(ep2X, ep2Y, 3, 0, Math.PI*2); ctx.fill();
       ctx.shadowBlur = 0;
     }
+    else if (this.type === 'psionic_slash') {
+      const rot = this.angle;
+      const alpha = Math.min(1, this.life / (this.maxLife * 0.3));
+      const currentRadius = (this.radius || 40) + (this.traveled || 0) * 0.5;
+      
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(rot);
+      
+      ctx.globalAlpha = alpha;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = 4 + currentRadius * 0.1;
+      ctx.shadowColor = this.color;
+      ctx.shadowBlur = 15;
+      
+      ctx.beginPath();
+      ctx.arc(0, 0, currentRadius, -Math.PI * 0.4, Math.PI * 0.4);
+      ctx.stroke();
+      
+      ctx.strokeStyle = '#fff';
+      ctx.lineWidth = 2 + currentRadius * 0.05;
+      ctx.shadowBlur = 0;
+      ctx.beginPath();
+      ctx.arc(0, 0, currentRadius * 0.85, -Math.PI * 0.35, Math.PI * 0.35);
+      ctx.stroke();
+      
+      ctx.fillStyle = this.color;
+      ctx.globalAlpha = alpha * 0.3;
+      ctx.beginPath();
+      ctx.arc(0, 0, currentRadius, -Math.PI * 0.4, Math.PI * 0.4);
+      ctx.lineTo(-currentRadius * 0.6, 0);
+      ctx.fill();
+      
+      ctx.restore();
+    }
     else if (this.type === 'fireball') {
       if (this.trailPositions) {
         for (let t of this.trailPositions) {
@@ -293,6 +349,27 @@ export default class Projectile {
       grad.addColorStop(1, 'rgba(230,126,34,0)');
       ctx.fillStyle = grad; ctx.beginPath();
       ctx.arc(this.x, this.y, r, 0, Math.PI*2); ctx.fill();
+    }
+    else if (this.type === 'psionic_slash') {
+      if (this.vx !== undefined) {
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+      }
+      this.traveled = (this.traveled || 0) + this.speed * dt;
+      this.currentRadius = (this.radius || 40) + this.traveled * 0.5;
+
+      for (let e of gameInstance.enemies) {
+        if (!e.alive || this.hitIds.has(e)) continue;
+        const hitX = this.x + Math.cos(this.angle) * this.currentRadius * 0.3;
+        const hitY = this.y + Math.sin(this.angle) * this.currentRadius * 0.3;
+        if (Math.hypot(hitX - e.x, hitY - e.y) < e.size + this.currentRadius * 0.6) {
+          if (!this.ownerId || (gameInstance.net && gameInstance.net.me && this.ownerId === gameInstance.net.me.info.user)) gameInstance.dealDamage(e, this.damage, this.critChance);
+          this.hitIds.add(e);
+          gameInstance.spawnParticles(e.x, e.y, this.color, 6, 3);
+        }
+      }
+      
+      if (this.life <= 0 || this.x < -100 || this.x > gameInstance.gameW + 100 || this.y < -100 || this.y > gameInstance.gameH + 100) this.life = 0;
     }
     else if (this.type === 'bolt') {
       const r = this.radius || 6;
